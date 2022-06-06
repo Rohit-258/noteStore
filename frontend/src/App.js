140d5ba097a8ed9c4106073b6af859350a3f054d
Rohit-258 <57527363+Rohit-258@users.noreply.github.com>
2022-06-06 23:24:55 +0530

    started the dev branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,19 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import LandingPage from "./screens/LandingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-      <LandingPage />
+      <main>
+        <Routes>
+          <Route path="/landing" element={<LandingPage />} />
+        </Routes>
+      </main>
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
